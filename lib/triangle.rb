08b1cfb9e4c5f4ecac12c4eb81ca9422c2198d02
ianrich69420 <ian.orieko@student.moringaschool.com>
2023-02-24 11:35:29 +0300

    Completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
   # write code here
+  def initialize len1, len2, len3
+    @len1 = len1
+    @len2 = len2
+    @len3 = len3
+  end
+
+  def kind
+    lengths = [@len1, @len2, @len3].sort
+    if lengths[0] >= 0 && lengths[0] + lengths[1] > lengths[2]
+      if lengths.uniq.size == 2
+        return :isosceles
+      elsif lengths.uniq.size <= 1
+        return :equilateral
+      elsif lengths.uniq.size == lengths.size
+        return :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
